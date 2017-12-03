2e32e4a96c364bbbeb41f9542f9e0424d28d0c06
Niko <niko.caron90@gmail.com>
2017-12-03 15:47:12 -0400

    Barebones App

---
+++ src/reducer.js
@@ -0,0 +1,9 @@
+import * as types from './actionTypes';
+import initialState from './initialState';
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
