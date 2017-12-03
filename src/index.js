b0f25b03bdc263104469f3cfa790426ca428c53e
Niko <niko.caron90@gmail.com>
2017-12-03 15:59:43 -0400

    Changed the structure a bit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from './reducer';
-import App from './App/App';
+import App from './Components/App';
 import 'reset-css';
 
 const store = createStore(reducer);
