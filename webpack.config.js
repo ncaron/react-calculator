7ac8904b1d1adfff9ac19deb6092617d1ff15fd7
Niko <niko.caron90@gmail.com>
2017-12-03 15:22:42 -0400

    Added redux

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,7 @@ module.exports = {
   devServer: {
     contentBase: './dist'
   },
-  entry: {
-    app: './src/index.js'
-  },
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
