c351fb797b9456c9f6900b196e9f1aa74dea8eea
Niko <niko.caron90@gmail.com>
2017-12-05 19:16:21 -0400

    Added the footer

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Calculator from '../Containers/Calculator';
 import Error from '../Components/Error';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <div>
       <Error />
       <Calculator />
+      <Footer />
     </div>
   );
 };
---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Made by <a href='https://github.com/ncaron' target='_blank'>Niko Caron</a>
+    </footer>
+  );
+}
+
+export default Footer;
